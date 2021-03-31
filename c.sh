a0d92e3d749e15769ca161d5f1b19297d90e99ee
Mu Young Ko <muyoungko@gmail.com>
2021-03-31 15:54:20 +0900

    a

--- c.sh
+++ c.sh
@@ -1,2 +1,4 @@
 #pod trunk register muyoungko@gmail.com muyoungko --description='Devil SDK'
+pod trunk push devilcore.podspec --allow-warnings
+pod trunk push devillogin.podspec --allow-warnings
 pod trunk push devil.podspec --allow-warnings
