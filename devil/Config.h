6f83974d353e14cafb6b77fbc244bd1516082f98
Mu Young Ko <muyoungko@gmail.com>
2021-04-01 16:22:52 +0900

    a

---
+++ devil/Config.h
@@ -0,0 +1,13 @@
+//
+//  Config.h
+//  devil
+//
+//  Created by Mu Young Ko on 2021/04/01.
+//  Copyright © 2021 Mu Young Ko. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+
+#endif /* Config_h */
