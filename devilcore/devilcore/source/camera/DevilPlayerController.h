5fd7fe047e4a12dd8cc975022f991a1048e3810b
Mu Young Ko <muyoungko@gmail.com>
2021-12-04 21:22:22 +0900

    a

---
+++ devilcore/devilcore/source/camera/DevilPlayerController.h
@@ -0,0 +1,16 @@
+//
+//  DevilPlayerController.h
+//  devilcore
+//
+//  Created by Mu Young Ko on 2021/12/04.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DevilPlayerController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ devilcore/devilcore/source/camera/DevilPlayerController.m
@@ -0,0 +1,12 @@
+//
+//  DevilPlayerController.m
+//  devilcore
+//
+//  Created by Mu Young Ko on 2021/12/04.
+//
+
+#import "DevilPlayerController.h"
+
+@implementation DevilPlayerController
+
+@end
