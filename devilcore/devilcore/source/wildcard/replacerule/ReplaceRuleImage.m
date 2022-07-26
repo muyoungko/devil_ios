58eb1fee761055327ab37438e9d3afc74a059edf
Mu Young Ko <muyoungko@gmail.com>
2022-07-26 23:03:12 +0900

    flip

--- devilcore/devilcore/source/wildcard/replacerule/ReplaceRuleImage.m
+++ devilcore/devilcore/source/wildcard/replacerule/ReplaceRuleImage.m
@@ -14,24 +14,47 @@
 
 @interface ReplaceRuleImage()
 @property (nonatomic, retain) NSString* currentUrl;
+@property (nonatomic, retain) UIImageView* flipImageView;
 @end
 
 @implementation ReplaceRuleImage
 
 - (void)constructRule:(WildCardMeta *)wcMeta parent:(UIView *)parent vv:(WildCardUIView *)vv layer:(id)layer depth:(int)depth result:(id)result{
+    {
+        UIView* iv = [[WildCardConstructor sharedInstance].delegate getNetworkImageViewInstnace];
+        self.replaceView = iv;
+        if(self.replaceJsonLayer[@"scaleType"] && [@"center_inside" isEqualToString:self.replaceJsonLayer[@"scaleType"]])
+            iv.contentMode = UIViewContentModeScaleAspectFit;
+        else
+            iv.contentMode = UIViewContentModeScaleAspectFill;
+        
+        [vv addSubview:iv];
+        [WildCardConstructor followSizeFromFather:vv child:iv];
+        
+        iv.layer.cornerRadius = vv.layer.cornerRadius;
+        iv.layer.maskedCorners = vv.superview.layer.maskedCorners;
+    }
     
-    UIView* iv = [[WildCardConstructor sharedInstance].delegate getNetworkImageViewInstnace];
-    self.replaceView = iv;
-    iv.contentMode = UIViewContentModeScaleAspectFill;
-    [vv addSubview:iv];
-    [WildCardConstructor followSizeFromFather:vv child:iv];
-    
-    self.replaceView.layer.cornerRadius = self.replaceView.superview.layer.cornerRadius;
-    self.replaceView.layer.maskedCorners = self.replaceView.superview.layer.maskedCorners;
-    
+    if(self.replaceJsonLayer[@"flipImageContent"])
+    {
+        UIView* iv = [[WildCardConstructor sharedInstance].delegate getNetworkImageViewInstnace];
+        self.flipImageView = (UIImageView*)iv;
+        if(self.replaceJsonLayer[@"scaleType"] && [@"center_inside" isEqualToString:self.replaceJsonLayer[@"scaleType"]])
+            iv.contentMode = UIViewContentModeScaleAspectFit;
+        else
+            iv.contentMode = UIViewContentModeScaleAspectFill;
+        
+        [vv addSubview:iv];
+        [WildCardConstructor followSizeFromFather:vv child:iv];
+        
+        iv.layer.cornerRadius = vv.layer.cornerRadius;
+        iv.layer.maskedCorners = vv.superview.layer.maskedCorners;
+        
+        [self performSelector:@selector(flip) withObject:nil afterDelay:2.0f];
+    }
 }
 
-- (void)updateRule:(WildCardMeta *)meta data:(id)opt{
+- (void)updateRule:(WildCardMeta *)meta data:(id)opt {
     NSString* url = nil;
     if([[opt class] isSubclassOfClass:[NSString class]])
         url = (NSString*)opt;
@@ -41,21 +64,52 @@
     }
     
     
-    if(url == nil){
-        [(UIImageView*)self.replaceView setImage:nil];
-        [(UIImageView*)self.replaceView setNeedsDisplay];
+    [self updateImageView:url view:(UIImageView*)self.replaceView meta:meta data:opt];
+    self.currentUrl = url;
+    if(self.currentUrl)
+        ((WildCardUIView*)[self.replaceView superview]).tags[@"url"] = url;
+    else
         [((WildCardUIView*)[self.replaceView superview]).tags removeObjectForKey:@"url"];
-        return;
+    
+    if(self.flipImageView) {
+        [self updateImageView:[MappingSyntaxInterpreter interpret:self.replaceJsonLayer[@"flipImageContent"]:opt] view:(UIImageView*)self.flipImageView meta:meta data:opt];
+        self.flipImageView.alpha = 0.0f;
+    }
+    
+}
+
+- (void)flip {
+    
+    [UIView animateWithDuration:0.30f delay:0.0f options:UIViewAnimationOptionCurveEaseOut animations:^{
+        if(self.replaceView.alpha == 1.0f){
+            self.replaceView.alpha = 0.0f;
+            self.flipImageView.alpha = 1.0f;
+        } else {
+            self.replaceView.alpha = 1.0f;
+            self.flipImageView.alpha = 0.0f;
+        }
+    } completion:^(BOOL finished) {
+//        NSLog(@"animate %@" , self.currentUrl);
+        [self performSelector:@selector(flip) withObject:nil afterDelay:2.0f];
+    }];
+}
+
+- (NSString*)updateImageView:(NSString*)url view:(UIImageView*)imageView meta:(WildCardMeta *)meta data:(id)opt {
+    
+    if(url == nil){
+        [imageView setImage:nil];
+        [imageView setNeedsDisplay];
+        return self.currentUrl;
     }
     
     if([url isEqualToString:self.currentUrl])
-        return;
+        return self.currentUrl;
     
-    [(UIImageView*)self.replaceView setImage:nil];
-    [(UIImageView*)self.replaceView setNeedsDisplay];
+    [imageView setImage:nil];
+    [imageView setNeedsDisplay];
     
     if([url hasPrefix:@"/"]) {
-        [(UIImageView*)self.replaceView setImage:[UIImage imageWithContentsOfFile:url]];
+        [imageView setImage:[UIImage imageWithContentsOfFile:url]];
     } else if([url hasPrefix:@"gallery://"]) {
         if(![url isEqualToString:self.currentUrl]) {
             PHImageRequestOptions* requestOptions = [[PHImageRequestOptions alloc] init];
@@ -75,17 +129,16 @@
                                   contentMode:PHImageContentModeAspectFill
                                       options:requestOptions
                                 resultHandler:^void(UIImage *image, NSDictionary *info) {
-                                    [(UIImageView*)self.replaceView setImage:image];
+                                    [imageView setImage:image];
                 }];
                 *stop = true;
             }];
         }
     } else {
-        [[WildCardConstructor sharedInstance].delegate loadNetworkImageView:self.replaceView withUrl:url];
-        ((WildCardUIView*)[self.replaceView superview]).tags[@"url"] = url;
+        [[WildCardConstructor sharedInstance].delegate loadNetworkImageView:imageView withUrl:url];
     }
     
-    self.currentUrl = url;
+    return url;
 }
 
 @end
