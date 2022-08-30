04ee4e389df9fee7cc2b0f5fb022f4cd38f9dcd1
Mu Young Ko <muyoungko@gmail.com>
2022-08-30 19:37:02 +0900

    a

--- devilcore/devilcore/source/devil/core/DevilBaseController.m
+++ devilcore/devilcore/source/devil/core/DevilBaseController.m
@@ -63,6 +63,7 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textEditing:) name:UITextFieldTextDidBeginEditingNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textEditing:) name:UITextViewTextDidBeginEditingNotification object:nil];
     [self updateFlexScreen];
 }
 
