0fdb61f67cce654f4ebd2b55b48fd2f6b7b0c552
Mu Young Ko <muyoungko@gmail.com>
2023-07-13 00:18:47 +0900

    a

--- devilcore/devilcore/source/wildcard/replacerule/ReplaceRuleLocalImage.m
+++ devilcore/devilcore/source/wildcard/replacerule/ReplaceRuleLocalImage.m
@@ -17,7 +17,7 @@
     if([WildCardConstructor sharedInstance].onLineMode && ![WildCardConstructor sharedInstance].localImageMode)
     {
         UIView* iv = [[WildCardConstructor sharedInstance].delegate getNetworkImageViewInstnace];
-        iv.contentMode = UIViewContentModeScaleAspectFill;
+        iv.contentMode = UIViewContentModeScaleToFill;
         [vv addSubview:iv];
         [WildCardConstructor followSizeFromFather:vv child:iv];
         //[outRules addObject:ReplaceRuleLocalImage(iv, layer, [layer objectForKey:@"localImageContent"])];
@@ -33,7 +33,7 @@
         NSString* imageName = [layer objectForKey:@"localImageContent"];
         NSUInteger index = [imageName rangeOfString:@"/" options:NSBackwardsSearch].location;
         imageName = [imageName substringFromIndex:index+1];
-        NSString* imgData = [WildCardConstructor getLocalFile:[NSString stringWithFormat:@"assets/images/%@", imageName]];
+        NSData* imgData = [WildCardConstructor getLocalFile:[NSString stringWithFormat:@"assets/images/%@", imageName]];
         [iv setImage: [UIImage imageWithData:imgData]];
         [WildCardConstructor followSizeFromFather:vv child:iv];
     }
--- devilnfc.podspec
+++ devilnfc.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
   s.name             = 'devilnfc'
   s.platform         = :ios
-  s.version          = '0.0.232'
+  s.version          = '0.0.233'
   s.summary          = 'Devil Nfc'
   s.description      = <<-DESC
     This is Devil Login
